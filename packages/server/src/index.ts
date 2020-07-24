efc5312194205c6aca05bf1e5effe8ecde7a6bff
Tomáš Vavřinka <email@tomasvavrinka.cz>
2020-07-24 14:00:16 +0200

    file upload

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,9 +1,25 @@
 import express from 'express';
+import upload from 'express-fileupload';
 
 const PORT = 4000;
 
 const app = express();
 
-app.get('/', (req, res) => res.json({ hello: 'world' }));
+app.use(upload());
+
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+app.post('/', (req, res) => {
+  const file = req.files?.file;
+
+  if (file && !(file instanceof Array)) {
+    file.mv('./uploads/' + file.name, (err: any) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send('File uploaded');
+      }
+    });
+  }
+});
 
 app.listen(PORT, () => console.log('ready - started server on http://localhost:%s', PORT));

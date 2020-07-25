a01388b5624ef87f30a9ddf80a13efd57a4446ec
Tomáš Vavřinka <email@tomasvavrinka.cz>
2020-07-25 12:18:03 +0200

    next upload form

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import routes from './routes';
+import cors from 'cors';
 
 const PORT = 4000;
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(cors());
+app.use((res, req, next) => setTimeout(next, 500));
 app.use('/', routes);
 
 app.listen(PORT, () => console.log('ready - started server on http://localhost:%s', PORT));
--- packages/server/src/routes/index.ts
+++ packages/server/src/routes/index.ts
@@ -1,22 +1,21 @@
 import express from 'express';
-import fs from 'fs';
 
 const router = express.Router();
 
 router.get('/', (req, res) => res.send('Home'));
-router.get('/test', (req, res) => res.send('Test'));
+router.post('/upload', (req, res) => res.send('Test'));
 
-router.get('/my-file/:fileName', (req, res) => {
-  fs.readFile('uploads/' + req.params.fileName, (err, data) => {
-    if (err) {
-      res.send('Some thing went wrong');
-    }
-    if (data) {
-      res.send(data);
-    } else {
-      res.send('File not found');
-    }
-  });
-});
+// router.get('/my-file/:fileName', (req, res) => {
+//   fs.readFile('uploads/' + req.params.fileName, (err, data) => {
+//     if (err) {
+//       res.send('Some thing went wrong');
+//     }
+//     if (data) {
+//       res.send(data);
+//     } else {
+//       res.send('File not found');
+//     }
+//   });
+// });
 
 export default router;

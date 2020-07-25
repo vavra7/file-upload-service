bcd9e6985fce796cee5587dc9e4d8ff627c0c515
Tomáš Vavřinka <email@tomasvavrinka.cz>
2020-07-25 12:18:19 +0200

    next upload form

---
+++ packages/client/src/api/index.ts
@@ -0,0 +1,6 @@
+import { AxiosRequestConfig } from 'axios';
+
+export const uploadRequest: AxiosRequestConfig = {
+  method: 'POST',
+  url: 'http://localhost:4000/upload'
+};
--- packages/client/src/pages/index.tsx
+++ packages/client/src/pages/index.tsx
@@ -1,7 +1,41 @@
 import { NextPage } from 'next';
+import axios from 'axios';
+import { uploadRequest } from '../api';
+import { useState, FormEvent, ChangeEvent } from 'react';
 
 const index: NextPage = () => {
-  return <div>here</div>;
+  const [file, setFile] = useState<File | undefined>();
+
+  const onFileSelect = (e: ChangeEvent<HTMLInputElement>): void => {
+    if (e.target.files?.length) {
+      setFile(e.target.files[0]);
+    }
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    console.log('file', file);
+
+    axios(uploadRequest)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <h1>File Upload</h1>
+
+        <input type="file" onChange={onFileSelect} />
+
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
 };
 
 export default index;
